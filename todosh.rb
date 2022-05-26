ea3b0f0522529ddddc632bd7477de03d4154e5da
Jon Linkens <jonlinkens@outlook.com>
2022-05-26 18:20:04 +0100

    fix hash value

--- todosh.rb
+++ todosh.rb
@@ -2,7 +2,7 @@ class Todosh < Formula
   desc "A lightweight CLI to do list manager"
   homepage "https://github.com/JonLinkens/todosh-rs"
   url "https://github.com/JonLinkens/todosh-rs/releases/download/v1.0.0/todosh-1.0.0-x86_64-apple-darwin.tar.gz"
-  sha256 "dc81d5ed200de4266b14c6a63135ccb65c9afc3d"
+  sha256 "eff33fa8e0a5bf7c53fec820f6cdf4b60a6cf11d1bab72c082de8b7471f95917"
   version "1.0.0"
 
   def install
